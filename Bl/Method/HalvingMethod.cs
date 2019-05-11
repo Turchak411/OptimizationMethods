7595fae55d127620ac80eb3f863203efbc22a45c
Андрей <ande5@mail.ru>
2019-05-11 18:41:52 +0300

    Add class HalvingMethod and Project FunctionCalculation

---
+++ Bl/Method/HalvingMethod.cs
@@ -0,0 +1,81 @@
+﻿namespace Bl.Method
+{
+    public class HalvingMethod
+    {
+        private readonly SingleVariableFunctionDelegate _f;
+
+        private IterationInfoEventArgs _iterationInfoEventArgs;
+
+        /// <summary>
+        /// Левая граница
+        /// </summary>
+        public double LeftBound => _iterationInfoEventArgs.LeftBound;
+
+        /// <summary>
+        /// Правая граница
+        /// </summary>
+        public double RightBound => _iterationInfoEventArgs.RightBound;
+
+        /// <summary>
+        /// Кол-во итераций
+        /// </summary>
+        public int Iteration => _iterationInfoEventArgs.Iteration;
+
+        public double MiddleInterval(double leftBound, double rightBound) => (leftBound + rightBound) / 2;
+
+        public double IntervalLength(double leftBound, double rightBound) => rightBound - leftBound;
+
+        public double X1(double leftBound, double intervalLength) => leftBound + intervalLength / 4;
+
+        public double X2(double rightBound, double intervalLength) => rightBound - intervalLength / 4;
+
+        public HalvingMethod(SingleVariableFunctionDelegate function)
+        {
+            _f = function;
+        }
+
+        /// <summary>
+        /// Вычисление методом деления пополам
+        /// </summary>
+        /// <param name="leftBound">Значение левой границы</param>
+        /// <param name="rightBound">Значение правой границы</param>
+        /// <returns>Средение значение x между границами</returns>
+        public double Calculation(double leftBound, double rightBound)
+        {
+            _iterationInfoEventArgs = new IterationInfoEventArgs(leftBound, rightBound, 0);
+            var middleInterval = MiddleInterval(leftBound, rightBound);
+            return RunIterations(_iterationInfoEventArgs, ref middleInterval);
+        }
+
+        /// <summary>
+        /// Выполнение итераций
+        /// </summary>
+        /// <param name="iterationInfo">Информация об итерациях</param>
+        /// <param name="middleInterval">Средение значение x</param>
+        /// <returns>Средение значение x между границами</returns>
+        private double RunIterations(IterationInfoEventArgs iterationInfo, ref double middleInterval)
+        {
+            var dx = IntervalLength(iterationInfo.LeftBound, iterationInfo.RightBound);
+            var x1 = X1(iterationInfo.LeftBound, dx);
+            var x2 = X2(iterationInfo.RightBound, dx);
+
+            if (_f(x1) < _f(middleInterval))
+            {
+                iterationInfo.RightBound = middleInterval;
+                middleInterval = x1;
+                iterationInfo.Iteration++;
+                RunIterations(iterationInfo, ref middleInterval);
+            }
+            else if (_f(x2) < _f(middleInterval))
+            {
+                iterationInfo.LeftBound = middleInterval;
+                middleInterval = x2;
+                iterationInfo.Iteration++;
+                RunIterations(iterationInfo, ref middleInterval);
+            }
+
+            return middleInterval;
+        }
+
+    }
+}
---
+++ FunctionCalculation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Bl.Method;
+
+namespace FunctionCalculation
+{
+    class Program
+    {
+        private static double leftBound = 1.5, rightBound = 7.5;
+
+        private static double MinimizationFunction(double x) => 2 * Math.Pow(x - 3, 2) + Math.Pow(Math.E, 0.5 * x);
+
+        static void Main(string[] args)
+        {
+            var halvingMethod = new HalvingMethod(MinimizationFunction);
+            var result = halvingMethod.Calculation(leftBound, rightBound);
+            Console.WriteLine("Границы: [{0}; {1}] Итераций = {2}", halvingMethod.LeftBound,
+                                               halvingMethod.RightBound, halvingMethod.Iteration);
+            Console.WriteLine("x = {0:f3}\nЗначение функции {1:f3}", result, MinimizationFunction(result));
+            Console.ReadKey();
+        }
+    }
+}
